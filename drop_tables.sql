c7bc0eeb7ae78edb209687c52c3b86078417417e
Sayid <Goldensquirrel11@gmail.com>
2024-06-16 19:34:36 +0800

    Made a drop all tables script for debugging

---
+++ drop_tables.sql
@@ -0,0 +1,11 @@
+-- A Script that drops all created tables
+-- This script was made purely for debugging purposes
+
+DROP TABLE IF EXISTS Booking_item;
+DROP TABLE IF EXISTS Booking_dependants;
+DROP TABLE IF EXISTS Booking;
+DROP TABLE IF EXISTS Dependants;
+DROP TABLE IF EXISTS Customer;
+DROP TABLE IF EXISTS Agent;
+DROP TABLE IF EXISTS Manager;
+DROP TABLE IF EXISTS Item;
