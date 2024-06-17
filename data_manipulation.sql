204d0b492ea895eb3fad676fc6a0bef6b8855e03
Sayid <Goldensquirrel11@gmail.com>
2024-06-17 18:04:10 +0800

    Change file format for data manipulation queries

---
+++ data_manipulation.sql
@@ -0,0 +1,10 @@
+-- Data manipulation query types
+-- 
+-- [0/2] Aggregate functions
+-- [0/1] VIEW
+-- [0/1] Subquery / Nested query
+-- [0/1] Triggers
+-- [0/1] Stored Procedure
+-- [0/4] Queries not covered in lecture/tutorial/lab
+-- 
+
