342517a0bda86f061de45f4d8354119d63c5d612
Sayid <Goldensquirrel11@gmail.com>
2024-06-18 10:37:20 +0800

    Created a new trigger for number_of_people
    update number of people in booking whenever an insert occurs

--- data_manipulation.sql
+++ data_manipulation.sql
@@ -1,10 +1,21 @@
 -- Data manipulation query types
 -- 
+-- Run this file using this command:
+-- $ db2 -td@ -f [filename]
+-- 
 -- [0/2] Aggregate functions
 -- [0/1] VIEW
 -- [0/1] Subquery / Nested query
--- [0/1] Triggers
+-- [1/1] Triggers
 -- [0/1] Stored Procedure
 -- [0/4] Queries not covered in lecture/tutorial/lab
 -- 
 
+
+-- update number of people in booking whenever an insert occurs
+CREATE OR REPLACE TRIGGER booking_number_of_people
+AFTER INSERT ON Booking
+FOR EACH ROW MODE DB2SQL
+BEGIN
+    UPDATE Booking b SET number_of_people = (SELECT COUNT(*) FROM Dependants d WHERE d.customer_id = b.customer_id);
+END@
